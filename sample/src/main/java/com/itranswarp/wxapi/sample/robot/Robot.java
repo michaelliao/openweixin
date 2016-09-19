7f43d1afc1dd2d3b7a8e9f71d5a6d948e155d8d2
Michael Liao <askxuefeng@gmail.com>
2016-09-19 18:00:49 +0800

    add aes encrypt

--- sample/src/main/java/com/itranswarp/wxapi/sample/robot/Robot.java
+++ sample/src/main/java/com/itranswarp/wxapi/sample/robot/Robot.java
@@ -1,10 +1,19 @@
 package com.itranswarp.wxapi.sample.robot;
 
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.security.Key;
+import java.util.Base64;
 import java.util.Map;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.itranswarp.wxapi.util.HashUtil;
 import com.itranswarp.wxapi.util.HttpUtil;
 import com.itranswarp.wxapi.util.JsonUtil;
 import com.itranswarp.wxapi.util.MapUtil;
@@ -14,24 +23,43 @@ public class Robot {
 
 	static final String TULING_URL = "http://www.tuling123.com/openapi/api";
 
-	@Value("${robot.app.id:}")
+	static final Map<String, String> HEADERS = MapUtil.createMap("Content-Type", "application/json");
+
+	@Value("${robot.app.key}")
 	String apiKey;
+
+	@Value("${robot.app.secret}")
+	String apiSecret;
 
 	public RobotResponse talk(String userId, String text) throws Exception {
 		return talk(userId, text, null);
 	}
 
 	public RobotResponse talk(String userId, String text, String location) throws Exception {
-		Map<String, String> params = MapUtil.createMap("key", apiKey, "userid", userId, "info", text);
+		Map<String, String> params = MapUtil.createMap("userid", userId, "info", text);
 		if (location != null && !location.isEmpty()) {
 			params.put("loc", location);
 		}
-		String json = HttpUtil.httpPost(TULING_URL, HttpUtil.urlEncode(params), null);
+		long timestamp = System.currentTimeMillis();
+		String jsonData = JsonUtil.toJson(params);
+		String aesKey = HashUtil.md5(apiSecret + timestamp + apiKey);
+		String encryptedData = aesEncrypt(aesKey, jsonData);
+		Map<String, Object> postData = MapUtil.createMap("key", apiKey, "timestamp", timestamp, "data", encryptedData);
+		String json = HttpUtil.httpPost(TULING_URL, JsonUtil.toJson(postData), HEADERS);
 		RobotResponse resp = JsonUtil.fromJson(RobotResponse.class, json);
 		if (resp.code >= 40000 && resp.code <= 49999) {
 			throw new RobotException(resp.code, resp.text);
 		}
 		return resp;
+	}
+
+	String aesEncrypt(String aesKey, String data) throws GeneralSecurityException {
+		Key key = new SecretKeySpec(aesKey.getBytes(), "AES");
+		IvParameterSpec iv = new IvParameterSpec(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+		byte[] encryptData = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
+		return Base64.getEncoder().encodeToString(encryptData);
 	}
 
 	public static class RobotResponse {
